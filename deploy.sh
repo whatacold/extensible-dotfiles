df03de9dd9d83dbd8447bb46de2cb0c066ea24f7
whatacold <whatacold@gmail.com>
2016-10-02 16:48:50 +0800

    keep tuning

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 ln -sf $PWD/astylerc ~/.astylerc
 
 # spf13-vim
-ln -sf $PWD/vimrc.local ~/.vimrc.local
+ln -sf $PWD/vimrc.before.local ~/.vimrc.before.local
 ln -sf $PWD/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf $PWD/vimrc.local ~/.vimrc.local
