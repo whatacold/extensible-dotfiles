c2eddbcafe050a1d53e60727b3f3876bfded754b
whatacold <whatacold@gmail.com>
2016-10-22 15:07:23 +0800

    custom oh-my-zsh.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-ln -sf $PWD/astylerc ~/.astylerc
-ln -sf $PWD/ackrc ~/.ackrc
+cd `dirname $0`
+
+ln -sf astylerc ~/.astylerc
+ln -sf ackrc ~/.ackrc
 
 # spf13-vim
-ln -sf $PWD/vimrc.before.local ~/.vimrc.before.local
-ln -sf $PWD/vimrc.bundles.local ~/.vimrc.bundles.local
-ln -sf $PWD/vimrc.local ~/.vimrc.local
+ln -sf vimrc.before.local ~/.vimrc.before.local
+ln -sf vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf vimrc.local ~/.vimrc.local
 
 # git
-ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf gitconfig ~/.gitconfig
+
+# oh-my-zsh
+ln -sf local.zsh ~/.oh-my-zsh/custom/local.zsh
