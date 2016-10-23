8c02d4ec841ad77f5a2159fb98feb8b7ef4bfb88
whatacold <whatacold@gmail.com>
2016-10-23 22:33:45 +0800

    bugfix: soft-links need absolute paths.

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,20 @@
 
 cd `dirname $0`
 
-ln -sf astylerc ~/.astylerc
-ln -sf ackrc ~/.ackrc
+ln -sf $PWD/astylerc ~/.astylerc
+ln -sf $PWD/ackrc ~/.ackrc
 
 # spf13-vim
-ln -sf vimrc.before.local ~/.vimrc.before.local
-ln -sf vimrc.bundles.local ~/.vimrc.bundles.local
-ln -sf vimrc.local ~/.vimrc.local
+ln -sf $PWD/vimrc.before.local ~/.vimrc.before.local
+ln -sf $PWD/vimrc.bundles.local ~/.vimrc.bundles.local
+ln -sf $PWD/vimrc.local ~/.vimrc.local
 
 # git
-ln -sf gitconfig ~/.gitconfig
+ln -sf $PWD/gitconfig ~/.gitconfig
 
 # oh-my-zsh
-ln -sf local.zsh ~/.oh-my-zsh/custom/local.zsh
+ln -sf $PWD/local.zsh ~/.oh-my-zsh/custom/local.zsh
 
 # leo editor
 # not work yet.
-# ln -sf myLeoSettings.leo ~/.leo/myLeoSettings.leo
+# ln -sf $PWD/myLeoSettings.leo ~/.leo/myLeoSettings.leo
