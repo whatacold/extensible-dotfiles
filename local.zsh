7228760e41fb89c5731463d58f5435646a861d7e
whatacold <whatacold@gmail.com>
2016-10-24 09:56:58 +0800

    alias for flushing tags.

--- local.zsh
+++ local.zsh
@@ -8,3 +8,4 @@ eval `dircolors $V_DIRROOT/dircolors.256dark`
 # alias
 alias rsync='rsync -avz --exclude=".svn*" -e "ssh -p 36000"'
 alias j='jobs'
+alias flushtags='rm -f tags cscope.*; ctags -R; cscope -Rb'
